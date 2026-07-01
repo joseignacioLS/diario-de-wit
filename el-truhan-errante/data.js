ec04bb236c518ab567ede0c28a4c4d23f7cae03c
Jose <ls.joseignacio@gmail.com>
2026-07-01 09:59:22 +0000

    refactor js

---
+++ el-truhan-errante/data.js
@@ -0,0 +1,47 @@
+export const sessions = [
+    {
+        number: 1,
+        title: "De vuelta a la termita",
+        img: { src: "assets/imgs/Sesión01.webp", caption: undefined }
+    },
+    {
+        number: 2,
+        title: "Rubén Hernández, empresario",
+        img: { src: "assets/imgs/Sesión02.webp", caption: undefined }
+    },
+    {
+        number: 3,
+        title: "A su servicio, Herrete",
+        img: { src: "assets/imgs/Sesión03.webp", caption: undefined }
+    },
+    {
+        number: 4,
+        title: "¿Rubén o Javier?",
+        img: { src: "assets/imgs/Sesión04.webp", caption: undefined }
+    },
+    {
+        number: 5,
+        title: "¡Javier!",
+        img: { src: "assets/imgs/Sesión05.webp", caption: undefined }
+    },
+    {
+        number: 6,
+        title: "Gallardo, (asistente del) Rey de los Corsarios",
+        img: { src: "assets/imgs/Sesión06.webp", caption: undefined }
+    },
+    {
+        number: 7,
+        title: "Oh mi capitán",
+        img: { src: "assets/imgs/Sesión07.webp", caption: undefined }
+    },
+    {
+        number: 8,
+        title: "-",
+        img: { src: undefined, caption: "Por algún motivo esta sesión no hice diario" }
+    },
+    {
+        number: 9,
+        title: "El plan",
+        img: { src: "assets/imgs/Sesión09.webp", caption: undefined }
+    }
+]
---
+++ el-truhan-errante/load.js
@@ -0,0 +1,58 @@
+import { createImage, createElement } from "../js/render.js";
+import { sessions } from "./data.js";
+
+const renderMenu = () => {
+    const asideElement = document.querySelector("aside");
+    if (!asideElement) return;
+    const menuElement = createElement("nav");
+    const ulElement = createElement("ul");
+    sessions
+        .sort((a, b) => b.number - a.number)
+        .forEach(session => {
+            if (!session.title) {
+                return;
+            }
+            const liElement = createElement("li");
+            const aElement = createElement("a", {
+                href: `#sesion-${session.number}`,
+                textContent: `#${session.number} ${session.title}`
+            });
+            liElement.appendChild(aElement);
+            ulElement.appendChild(liElement);
+        });
+    menuElement.appendChild(ulElement);
+    asideElement.appendChild(menuElement);
+}
+
+const renderDiaryEntries = () => {
+    const mainElement = document.querySelector("main");
+    if (!mainElement) return;
+    sessions
+        .filter(({ title }) => title !== undefined)
+        .sort((a, b) => b.number - a.number)
+        .forEach(session => {
+            const articleElement = createElement("article", { class: "session" });
+            const titleElement = createElement("h3", {
+                class: "title",
+                textContent: `#${session.number} ${session.title}`,
+                id: `sesion-${session.number}`
+            });
+            articleElement.appendChild(titleElement);
+            if (session.img) {
+                const imgElement = createImage(
+                    session.img.src,
+                    session.img.caption,
+                    `Fotografía del la ${session.number} página del diario del Truhan Errante`
+                );
+                articleElement.appendChild(imgElement);
+            }
+            mainElement.appendChild(articleElement);
+        });
+}
+
+const render = async () => {
+    renderDiaryEntries();
+    renderMenu();
+};
+
+render();
