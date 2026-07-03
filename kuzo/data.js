be4cc3f044370ba82a7b323d2e051755ff772078
Jose <ls.joseignacio@gmail.com>
2026-07-03 08:56:02 +0000

    add kuzo end explanation

--- kuzo/data.js
+++ kuzo/data.js
@@ -2,21 +2,30 @@ export const sessions = [
     {
         session: 1,
         title: "Trasfondo",
-        body: [{ type: "img", src: "/trasfondo.webp", caption: undefined }]
+        body: [
+            { type: "img", src: "/trasfondo.webp", caption: undefined }
+        ]
     },
     {
         session: 2,
         title: "Sesión 1",
-        body: [{ type: "img", src: "/Sesion01-1.webp", caption: undefined }, { type: "img", src: "/Sesion01-2.webp", caption: undefined }]
+        body: [
+            { type: "img", src: "/Sesion01-1.webp", caption: undefined },
+            { type: "img", src: "/Sesion01-2.webp", caption: undefined }]
     },
     {
         session: 3,
         title: "Sesión 2",
-        body: [{ type: "img", src: "/Sesion02.webp", caption: undefined }]
+        body: [
+            { type: "img", src: "/Sesion02.webp", caption: undefined }
+        ]
     },
     {
         session: 4,
         title: "Sesión 3",
-        body: [{ type: "img", src: "/Sesion03.webp", caption: undefined }]
+        body: [
+            { type: "img", src: "/Sesion03.webp", caption: undefined },
+            { type: "p", text: "Por desgracia Kuzo no tiene un final feliz. La última pelea fue letal. Dio su vida por proteger a dos personas a las que dos días antes llamaba extraños ante un ser despiadado que carbonizó sus restos." }
+        ]
     },
 ]
