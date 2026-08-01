030f1898ba1b1a9907f54c04376ef333378e1df4
Jose <ls.joseignacio@gmail.com>
2026-08-01 13:36:10 +0200

    add lcda maps

--- assets/data/games.js
+++ assets/data/games.js
@@ -2,56 +2,66 @@ import { sessions as ElDiarioDeWit } from "./sessions/el-diario-de-wit.js";
 import { sessions as ElTruhanErrante } from "./sessions/el-truhan-errante.js";
 import { sessions as Kuzo } from "./sessions/kuzo.js";
 import { sessions as LaEscupidera } from "./sessions/la-escupidera.js";
+import { sessions as LCDA } from "./sessions/lcda.js";
 
-export const games = [
-    {
-        title: "El Festival de las Bestias",
-        tag: "festival-de-las-bestias",
-        startDate: "2026",
-        endDate: "Actualidad",
-        order: 1,
-        summary: "La pequeña ciudad de Elmore ha perdido su defensa contra las bestias. El grupo debe aventurarse en las cavernas para tratar de arreglar el sistema, ¡y cocinar deliciosas recetas!",
-        tags: ["GM", "DaggerHeart"],
-        sessions: []
-    },
-    {
-        title: "El Truhan Errante",
-        tag: "el-truhan-errante",
-        startDate: "2025",
-        endDate: "2026",
-        order: 1,
-        summary: "El Truhan Errante es el nombre del barco en el que Cornelio Tramontana, en el puerto de Marea Baja. El capitán Damián ha encomendado a Cornelio, junto a sus compañeros Rufus y Silvestre, que pongan a la tripulación del Truhan Errante de vuelta en altamar, consiguiendo una reparación... o quizás un nuevo barco.",
-        tags: ["Jugador", "BRP"],
-        sessions: ElTruhanErrante
-    },
-    {
-        title: "Kuzo",
-        tag: "kuzo",
-        startDate: "2025",
-        endDate: "2025",
-        order: 2,
-        summary: "Kuzo es un clérigo Kenku que junto a dos aventureros, trata de encontrar su destino en la senda de Mask.",
-        tags: ["Jugador", "D&D"],
-        sessions: Kuzo
-    },
-    {
-        title: "La Escupidera",
-        tag: "la-escupidera",
-        startDate: "2020",
-        endDate: "2024",
-        order: 3,
-        summary: "Este es el resumen desde mi punto de vista como <em>Game Master</em> de una partida de vaqueros. La protagonizan Alain, Vince y Will, tres vaqueros que trabajan en un rancho dirigido por Tomas. Entre sus quehaceres se encuentra cuidar de las reses, mantener el rancho y pelearse con sus acerrimos enemigos, Wyatt y sus muchachos, del otro rancho. Todo cambia cuando Tomas adquiere de propiedad de una mina abandonada hace años.",
-        tags: ["GM", "Call of Cthulhu"],
-        sessions: LaEscupidera
-    },
-    {
-        title: "El Diario de Wit",
-        tag: "el-diario-de-wit",
-        startDate: "2010",
-        endDate: "?",
-        order: 4,
-        summary: "Por desgracia el diario de Wit original se ha perdido en el tiempo. Tengo algunas cosas sueltas, pero no para recomponer la campaña que jugué en su momento.",
-        tags: ["Jugador", "D&D"],
-        sessions: ElDiarioDeWit
-    }
+export const games = [{
+    title: "Los Cazadores de Aurovita",
+    tag: "lcda",
+    startDate: "2025",
+    endDate: "Actualidad",
+    order: 1,
+    summary: "Recopilación de los mapas pintados a mano",
+    tags: ["GM", "D&D"],
+    sessions: LCDA
+},
+{
+    title: "El Festival de las Bestias",
+    tag: "festival-de-las-bestias",
+    startDate: "2026",
+    endDate: "Actualidad",
+    order: 1,
+    summary: "La pequeña ciudad de Elmore ha perdido su defensa contra las bestias. El grupo debe aventurarse en las cavernas para tratar de arreglar el sistema, ¡y cocinar deliciosas recetas!",
+    tags: ["GM", "DaggerHeart"],
+    sessions: []
+},
+{
+    title: "El Truhan Errante",
+    tag: "el-truhan-errante",
+    startDate: "2025",
+    endDate: "2026",
+    order: 1,
+    summary: "El Truhan Errante es el nombre del barco en el que Cornelio Tramontana, en el puerto de Marea Baja. El capitán Damián ha encomendado a Cornelio, junto a sus compañeros Rufus y Silvestre, que pongan a la tripulación del Truhan Errante de vuelta en altamar, consiguiendo una reparación... o quizás un nuevo barco.",
+    tags: ["Jugador", "BRP"],
+    sessions: ElTruhanErrante
+},
+{
+    title: "Kuzo",
+    tag: "kuzo",
+    startDate: "2025",
+    endDate: "2025",
+    order: 2,
+    summary: "Kuzo es un clérigo Kenku que junto a dos aventureros, trata de encontrar su destino en la senda de Mask.",
+    tags: ["Jugador", "D&D"],
+    sessions: Kuzo
+},
+{
+    title: "La Escupidera",
+    tag: "la-escupidera",
+    startDate: "2020",
+    endDate: "2024",
+    order: 3,
+    summary: "Este es el resumen desde mi punto de vista como <em>Game Master</em> de una partida de vaqueros. La protagonizan Alain, Vince y Will, tres vaqueros que trabajan en un rancho dirigido por Tomas. Entre sus quehaceres se encuentra cuidar de las reses, mantener el rancho y pelearse con sus acerrimos enemigos, Wyatt y sus muchachos, del otro rancho. Todo cambia cuando Tomas adquiere de propiedad de una mina abandonada hace años.",
+    tags: ["GM", "Call of Cthulhu"],
+    sessions: LaEscupidera
+},
+{
+    title: "El Diario de Wit",
+    tag: "el-diario-de-wit",
+    startDate: "2010",
+    endDate: "?",
+    order: 4,
+    summary: "Por desgracia el diario de Wit original se ha perdido en el tiempo. Tengo algunas cosas sueltas, pero no para recomponer la campaña que jugué en su momento.",
+    tags: ["Jugador", "D&D"],
+    sessions: ElDiarioDeWit
+}
 ];
---
+++ assets/data/sessions/lcda.js
@@ -0,0 +1,27 @@
+export const sessions = [
+    {
+        arc: 2,
+        session: 58,
+        title: "El Laboratorio de Joana",
+        body: [{
+            type: "img",
+            src: [
+                { src: "@/assets/imgs/lcda/LaboratorioJoana.webp", media: `(width > 512px)`, default: true }
+            ],
+            alt: "Dibujo del mapa de combate del Laboratorio de Joana",
+            caption: undefined
+        }]
+    }, {
+        arc: 2,
+        session: 43,
+        title: "Las Alcantarillas de Noos",
+        body: [{
+            type: "img",
+            src: [
+                { src: "@/assets/imgs/lcda/AlcantarillasNoos.webp", media: `(width > 512px)`, default: true }
+            ],
+            alt: "Dibujo del mapa de combate de las alcantarillas de Noos",
+            caption: undefined
+        }]
+    },
+];
